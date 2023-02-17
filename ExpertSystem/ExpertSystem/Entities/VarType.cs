66ffe3dd05d7e74e033fe7c6ee5c47fb025aac8d
gitbleidd <adonis7952@hotmail.com>
2023-02-17 20:05:39 +0500

    add: Variable and VarType

---
+++ ExpertSystem/ExpertSystem/Entities/VarType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpertSystem.Entities
+{
+    public enum VarType
+    {
+        Inquire,
+        Produce,
+        InquireProduce,
+        ProduceInquire
+    }
+}
--- ExpertSystem/ExpertSystem/Entities/Variable.cs
+++ ExpertSystem/ExpertSystem/Entities/Variable.cs
@@ -8,5 +8,19 @@ namespace ExpertSystem.Entities
 {
     public class Variable
     {
+        public string Name { get; set; }
+        public Domain Value { get; set; }
+        public VarType VariableType { get; set; }
+        public string QuestionText { get; set; }
+
+        public Variable(string name, Domain value, VarType variableType, string questionText)
+        {
+            Name = name;
+            Value = value;
+            VariableType = variableType;
+            QuestionText = questionText;
+        }
+
+        public override string ToString() => Name;
     }
 }
