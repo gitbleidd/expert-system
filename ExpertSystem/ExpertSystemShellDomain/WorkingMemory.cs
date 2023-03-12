4d943b7c0ede0cdb45627a7790299a8869626d59
gitbleidd <adonis7952@hotmail.com>
2023-03-12 15:43:28 +0500

    add: target variable in working memory

--- ExpertSystem/ExpertSystemShellDomain/WorkingMemory.cs
+++ ExpertSystem/ExpertSystemShellDomain/WorkingMemory.cs
@@ -6,10 +6,19 @@ public class WorkingMemory
 {
     public List<Rule> FiredRules { get; }
     public Dictionary<Variable, DomainValue> VariableValues { get; }
+    public Variable? TargetVariable { get; }
     
     public WorkingMemory()
     {
         FiredRules = new List<Rule>();
         VariableValues = new Dictionary<Variable, DomainValue>();
+        TargetVariable = null;
+    }
+
+    public WorkingMemory(Variable targetVariable)
+    {
+        FiredRules = new List<Rule>();
+        VariableValues = new Dictionary<Variable, DomainValue>();
+        TargetVariable = targetVariable;
     }
 }
